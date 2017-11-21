e6a493fe8fb6d6a80ea2f35f77859ba81290753f
mannycolon <colonmanuel7@gmail.com>
2017-11-20 23:27:25 -0500

    created cachedFonts helper

---
+++ src/helpers/cachedFonts.js
@@ -0,0 +1,19 @@
+import { Font } from 'expo';
+
+export const cachedFonts = fonts =>
+  fonts.map(font => Font.loadAsync(font));
+
+export const fontAssets = cachedFonts([
+  {
+    montserrat: require('../../assets/fonts/Montserrat-Regular.ttf'),
+  },
+  {
+    montserratBold: require('../../assets/fonts/Montserrat-Bold.ttf'),
+  },
+  {
+    montserratLight: require('../../assets/fonts/Montserrat-Light.ttf'),
+  },
+  {
+    montserratMed: require('../../assets/fonts/Montserrat-Medium.ttf'),
+  },
+]);
