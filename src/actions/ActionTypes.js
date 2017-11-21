9e7d44c873db44421ca2429fa37fdadf1b4db31a
mannycolon <colonmanuel7@gmail.com>
2017-11-20 23:06:38 -0500

    added logout action

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -2,6 +2,7 @@ const ActionTypes = {
   LOGIN: 'LOGIN',
   LOGIN_SUCCESS: 'LOGIN_SUCCESS',
   LOGIN_ERROR: 'LOGIN_ERROR',
+  LOGOUT: 'LOGOUT',
 };
 
 export default ActionTypes;
---
+++ src/actions/LoginActions.js
@@ -0,0 +1,36 @@
+import ActionTypes from './ActionTypes';
+import { User } from '../constants/api';
+
+function loginSuccess(data) {
+  return {
+    type: ActionTypes.LOGIN_SUCCESS,
+    user: data.user,
+    token: data.token,
+  };
+}
+
+function loginError(error) {
+  return {
+    type: ActionTypes.LOGIN_ERROR,
+    error,
+  };
+}
+
+export function login(token, provider) {
+  return async (dispatch) => {
+    dispatch({ type: ActionTypes.LOGIN });
+    try {
+      const data = await User.login({ token, provider });
+      // Because of await once the previous line is done it jumps to the next line.
+      return dispatch(loginSuccess(data));
+    } catch (error) {
+      return dispatch(loginError(error));
+    }
+  };
+}
+
+export function logout() {
+  return {
+    type: ActionTypes.LOGOUT,
+  };
+}
