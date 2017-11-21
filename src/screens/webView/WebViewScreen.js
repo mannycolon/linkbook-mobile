a275922ae97bff2a854a085a5306db2b2ca711d5
mannycolon <colonmanuel7@gmail.com>
2017-11-20 23:17:47 -0500

    removed BackAndroid from webViewScreen.js

--- src/screens/webView/WebViewScreen.js
+++ src/screens/webView/WebViewScreen.js
@@ -1,31 +1,8 @@
 import React, { Component } from 'react';
-import { View, WebView, StyleSheet, BackAndroid } from 'react-native';
-import Spinner from '../../common/Spinner';
-import { Container, Header, Content, Toast, Button, Text, Icon } from 'native-base';
+import { View, WebView, StyleSheet } from 'react-native';
+import { LoadingScreen } from '../../commons';
 
 export default class WebViewScreen extends Component {
-  componentWillMount() {
-    BackAndroid.addEventListener('hardwareBackPress', this.handleBackButton);
-  }
-
-  componentWillUnMount() {
-    BackAndroid.removeEventListener('hardwareBackPress', this.handleBackButton);
-  }
-
-  handleBackButton() {
-    const { goBack, state } = this.props.navigation;
-    // if (_navigator.getCurrentRoutes().length === 1) {
-    //  return false;
-    // }
-    Toast.show({
-      text: 'Wrong$',
-      position: 'bottom',
-      buttonText: 'Okay',
-    });
-    goBack();
-    return true;
-  }
-
   render() {
     const { uri } = this.props.navigation.state.params;
 
@@ -37,7 +14,7 @@ export default class WebViewScreen extends Component {
             uri,
             method: 'GET',
           }}
-          renderLoading={() => <Spinner />}
+          renderLoading={() => <LoadingScreen />}
           javaScriptEnabledAndroid
           startInLoadingState
         />
