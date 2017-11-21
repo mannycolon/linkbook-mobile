7ec33226beae2773f4129d190a514184f172babc
mannycolon <colonmanuel7@gmail.com>
2017-11-20 23:09:11 -0500

    added new fonts to the app

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { AsyncStorage } from 'react-native';
+import { AppLoading } from 'expo';
+import { AsyncStorage, UIManager } from 'react-native';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
-import { AppLoading } from 'expo';
+// redux
 import store from './src/redux/store';
+// components
 import Root from './src/root';
+import { fontAssets } from './src/helpers';
+
+if (UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 export default class App extends React.Component {
   state = {
+    fontLoaded: false,
     ready: false,
   }
 
   componentDidMount() {
+    this._loadAssetsAsync();
     persistStore(
       store,
       {
@@ -24,8 +33,13 @@ export default class App extends React.Component {
     );
   }
 
+  async _loadAssetsAsync() {
+    await Promise.all(fontAssets);
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
-    if (!this.state.ready) {
+    if (!this.state.fontLoaded || !this.state.ready) {
       return <AppLoading />;
     }
     return (
