89e6b9c805510fd244535795fabaeee6337ee5a9
mannycolon <colonmanuel7@gmail.com>
2017-11-25 17:35:45 -0500

    fixed bug with user reducer local storage (data persistent)

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default class App extends React.Component {
       {
         storage: AsyncStorage,
         whitelist: [
-          'user',
+          'userReducer',
         ],
       },
       () => this.setState({ ready: true })
