7064cef9531130e256e8c0d206f46feb486726a2
mannycolon <colonmanuel7@gmail.com>
2017-12-09 01:45:31 -0500

    added new colors to colors constant

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -2,7 +2,9 @@ export default {
   blackColor: '#000000',
   whiteColor: '#ffffff',
   blackBlueColor: '#384259',
+  greyColor: '#b3b3b3',
   redColor: '#bd0826',
+  blueColor: '#007aff',
   signinButtonBackgroundColor: '#bd0826',
   signupButtonBackgroundColor: '#384259',
 };
