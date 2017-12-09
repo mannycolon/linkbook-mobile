1e1d4a9454e00b012c7cffed85fa7d922e9ce112
mannycolon <colonmanuel7@gmail.com>
2017-12-09 01:24:32 -0500

    created the PublicArticlesScreen and added fetching fucntionality

--- src/screens/publicArticles/PublicArticlesScreen.js
+++ src/screens/publicArticles/PublicArticlesScreen.js
@@ -1,23 +1,68 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { ScrollView, View, Text, Image } from 'react-native';// components
+import BoxShadow from '../../commons/BoxShadow';
+// actions
+import * as ArticlesActions from '../../actions/ArticlesActions';
 
-export default class PublicArticlesScreen extends Component {
+class PublicArticlesScreen extends Component {
+  componentDidMount() {
+    this.props.fetchPublicArticles();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const currentScreenIndex = this.props.navigationReducer.routes[0].index;
+    const newScreenIndex = nextProps.navigationReducer.routes[0].index;
+    if (currentScreenIndex !== 1 && newScreenIndex === 1) {
+      this.props.fetchPublicArticles();
+    }
+  }
+
   render() {
+    const {
+      articles,
+    } = this.props.publicArticles;
+
     return (
-      <View style={styles.container}>
-        <Text>PublicArticlesScreen</Text>
-      </View>
+      <ScrollView style={{ flex: 1 }}>
+        {
+          articles.map((article, index) => (
+            <BoxShadow key={index.toString()} onPress={() => this.props.navigation.navigate('WebView', article)}>
+              <Image source={{ uri: article.imageURL }} style={{ height: '100%', width: '45%', padding: 0, margin: 0 }} />
+              <View style={{ width: 0, flexGrow: 1 }}>
+                <Text style={{ padding: 10, fontWeight: 'bold', opacity: 0.9 }} ellipsizeMode='tail' numberOfLines={4}>
+                  {article.title}
+                </Text>
+              </View>
+            </BoxShadow>
+          ))
+        }
+      </ScrollView>
     );
   }
 }
 
-// PublicArticlesScreen.propTypes = {
-//   variable:	PropTypes.variableType.isRequired,
-// };
+PublicArticlesScreen.propTypes = {
+  publicArticles: PropTypes.shape({
+    articles: PropTypes.arrayOf(PropTypes.object).isRequired,
+    isFetched: PropTypes.bool.isRequired,
+    error: PropTypes.shape({
+      on: PropTypes.bool.isRequired,
+      message: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const mapStateToProps = (state) => ({
+  publicArticles: state.articlesReducer.publicArticles,
+  navigationReducer: state.navigationReducer,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchPublicArticles: () => {
+    dispatch(ArticlesActions.fetchPublicArticles());
   },
 });
+
+export default connect(mapStateToProps, mapDispatchToProps)(PublicArticlesScreen);
