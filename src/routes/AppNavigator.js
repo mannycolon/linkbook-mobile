631ca553c2d4531f6963f0bdd745dbdebd7a5854
mannycolon <colonmanuel7@gmail.com>
2017-12-10 01:03:38 -0500

    now Handling android back button on react navigation

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import { addNavigationHelpers } from 'react-navigation';
+import { Alert, BackHandler } from 'react-native';
+import { addNavigationHelpers, NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
-
 import Navigator from './Navigator';
 import { LoginScreen } from '../screens';
+// actions
+import * as ArticlesActions from '../actions/ArticlesActions';
 
 class AppNavigator extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.addArticleErrorMessage) {
+      Alert.alert('Error Adding New Article', nextProps.addArticleErrorMessage);
+      this.props.clearAddArticleErrorMessage();
+    }
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  onBackPress = () => {
+    const { dispatch, navigationReducer } = this.props;
+    if (navigationReducer.index === 0) {
+      return false;
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  };
+
   render() {
     const navigation = addNavigationHelpers({
       dispatch: this.props.dispatch,
@@ -22,11 +48,15 @@ class AppNavigator extends Component {
 const mapStateToProps = (state) => ({
   navigationReducer: state.navigationReducer,
   userReducer: state.userReducer,
+  addArticleErrorMessage: state.articlesReducer.addArticleErrorMessage,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   dispatch: (value) => {
     dispatch(value);
+  },
+  clearAddArticleErrorMessage: () => {
+    dispatch(ArticlesActions.clearAddArticleErrorMessage());
   },
 });
 
