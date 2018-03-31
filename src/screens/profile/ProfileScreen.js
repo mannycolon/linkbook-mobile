ba2b4a0b12ae3b37f1b04ac1b85168c778aa7ec9
mannycolon <colonmanuel7@gmail.com>
2018-03-31 18:37:06 -0400

    resedign the profile screen layout

--- src/screens/profile/ProfileScreen.js
+++ src/screens/profile/ProfileScreen.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { ScrollView, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Thumbnail, List, ListItem, Left, Right, Body, Icon } from 'native-base';
-import { FontAwesome } from '@expo/vector-icons';
 import Colors from '../../constants/Colors';
 // actions
 import * as LoginActions from '../../actions/LoginActions';
@@ -16,44 +15,45 @@ const ProfileScreen = ({
     fullName,
     email,
   },
+  navigation: {
+    navigate,
+  },
   logout,
 }) => (
-  <ScrollView style={{ display: 'flex', flex: 1, height: '100%', backgroundColor: Colors.redColor }}>
-    <View style={{ display: 'flex', flex: 1, height: '100%', justifyContent: 'space-between' }}>
-      <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Thumbnail large source={{ uri: avatar }} style={{ margin: 10, borderColor: Colors.whiteColor, borderWidth: 2 }} />
-        <Text style={{ fontWeight: 'bold', margin: 10, color: Colors.whiteColor }}>{fullName}</Text>
-        <Text style={{ fontWeight: 'bold', margin: 10, color: Colors.whiteColor }}>{email}</Text>
-      </View>
-      <List style={{ backgroundColor: Colors.redColor }}>
-        <ListItem icon style={{ backgroundColor: Colors.redColor }}>
-          <Left>
-            <Icon name="ios-settings" style={{ color: '#ffffff' }} />
-          </Left>
-          <Body>
-            <Text style={{ color: Colors.whiteColor }}>Settings</Text>
-          </Body>
-          <Right>
-            <Icon name="arrow-forward" style={{ color: '#ffffff' }} />
-          </Right>
-        </ListItem>
-        <ListItem icon onPress={() => logout()} style={{ backgroundColor: Colors.redColor }}>
-          <Left>
-            <FontAwesome size={23} name='sign-out' color='#ffffff' />
-          </Left>
-          <Body>
-            <Text style={{ color: Colors.whiteColor }}>Logout</Text>
-          </Body>
-          <Right>
-            <Icon name="arrow-forward" style={{ color: '#ffffff' }} />
-          </Right>
-        </ListItem>
-      </List>
-      <Text style={{ textAlign: 'center', fontWeight: 'bold', padding: 5, color: Colors.whiteColor }}>
-        LinkBook version: {pkg.version}
-      </Text>
+  <View style={{ display: 'flex', flex: 1, height: '100%', backgroundColor: Colors.redColor, justifyContent: 'space-between' }}>
+    <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: 20 }}>
+      <Thumbnail large source={{ uri: avatar }} style={{ margin: 10, borderColor: Colors.whiteColor, borderWidth: 2 }} />
+      <Text style={{ fontWeight: 'bold', margin: 10, color: Colors.whiteColor }}>{fullName}</Text>
+      <Text style={{ fontWeight: 'bold', margin: 10, color: Colors.whiteColor }}>{email}</Text>
     </View>
-  </ScrollView>
+    <List style={{ backgroundColor: Colors.redColor, paddingRight: 10 }}>
+      <ListItem icon onPress={() => navigate('AccountSettings')} style={{ backgroundColor: Colors.redColor }}>
+        <Left>
+          <Icon name="ios-settings" style={{ color: '#ffffff' }} />
+        </Left>
+        <Body>
+          <Text style={{ color: Colors.whiteColor }}>Settings</Text>
+        </Body>
+        <Right>
+          <Icon name="arrow-forward" style={{ color: '#ffffff' }} />
+        </Right>
+      </ListItem>
+      <ListItem icon onPress={() => logout()} style={{ backgroundColor: Colors.redColor }}>
+        <Left>
+          <Icon name='md-log-out' style={{ color: '#ffffff' }} />
+        </Left>
+        <Body>
+          <Text style={{ color: Colors.whiteColor }}>Logout</Text>
+        </Body>
+        <Right>
+          <Icon name="arrow-forward" style={{ color: '#ffffff' }} />
+        </Right>
+      </ListItem>
+    </List>
+    <Text style={{ textAlign: 'center', fontWeight: 'bold', padding: 5, color: Colors.whiteColor }}>
+        LinkBook version: {pkg.version}
+    </Text>
+  </View>
 );
 
 ProfileScreen.propTypes = {
